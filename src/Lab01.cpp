2172cf885511fe568c03fc09795c34ee9584c81d
Saddy14 <122515273+Saddy14@users.noreply.github.com>
2025-04-12 08:30:21 +0800

    Add Config, Test Files and readme update

---
+++ src/Lab01.cpp
@@ -0,0 +1,39 @@
+#include <GL/glut.h>
+// #include <GL/glxew.h>
+
+void myInit(void)
+{
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+  glColor3f(0.0, 0.0, 0.0);
+  glPointSize(4.0);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0,640.0,0,480.0);
+}
+//-------------------------------------------------------------------
+void myDisplay(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glBegin(GL_POINTS);
+  glVertex2f(100, 100);
+  
+  glEnd();
+
+  glFlush();
+}
+//-------------------------------------------------------------------
+int main( int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(640,480);
+  glutInitWindowPosition(100,100);
+  glutCreateWindow("Lab");
+  glutDisplayFunc(myDisplay);
+  myInit();
+
+  glutMainLoop();
+  return 0;
+}
